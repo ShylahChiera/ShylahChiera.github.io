0a3ad5150aaf65f4442254e75d067b6ab2d8c621
Shylah Chiera <shylah.chiera@gmail.com>
2018-08-28 16:28:50 -0400

    site maintenance memo

---
+++ _site/js/projectObject.js
@@ -0,0 +1,10 @@
+// var a = "hello";
+
+var obj = '{ "projects" : [' +
+'{ "name":"ColorCode" , "description":"Design, manage, and implement a React Native app which integrates Clarifai.api and The Color API to produce color schemes based off a chosen color.", "images": ["../sources/software/colorcode/Welcome.jpg","../sources/software/colorcode/choosephoto.jpg","../sources/software/colorcode/selectcolor.jpg", "../sources/software/colorcode/selecttype.jpg", "../sources/software/colorcode/share.jpg"]},' +
+'{ "name":"outlet.fyi" , "description":"Designed and implemented a robust application to help promote marketing strategies for entertainment events.", "images":["", "https://ibb.co/jTHSFp", "https://ibb.co/dRU7Fp", "https://ibb.co/cJpk1U"]},' +
+'{ "name":"Peter" , "desription":"Jones" } ]}';
+
+// var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+// ["https://ibb.co/iYBZT9","https://ibb.co/jbCf1U","https://ibb.co/cmn8o9", "https://ibb.co/iG8yo9", "https://ibb.co/mxUzvp"]
+// ["https://ibb.co/gb2G89", "https://ibb.co/jTHSFp", "https://ibb.co/dRU7Fp", "https://ibb.co/cJpk1U"]
